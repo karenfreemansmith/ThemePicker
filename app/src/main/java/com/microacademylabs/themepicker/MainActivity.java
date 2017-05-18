9aa2eb3c15a23a0bc3f812b4f8402c478070c824
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-05-17 20:03:26 -0700

    Initial commit

---
+++ app/src/main/java/com/microacademylabs/themepicker/MainActivity.java
@@ -0,0 +1,35 @@
+package com.microacademylabs.themepicker;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+  private Button btn;
+  private int myTheme;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    
+    btn = (Button) findViewById(R.id.changeButton);
+    btn.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+
+      }
+    });
+  }
+  @Override
+  public Resources.Theme getTheme() {
+    Resources.Theme theme = super.getTheme();
+    theme.applyStyle(R.style.AppTheme2, true);
+
+    return theme;
+  }
+}
