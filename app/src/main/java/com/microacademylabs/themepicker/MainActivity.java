eb0ad973dadf48f9345e7a65ff8ddae15f0c9e90
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-05-17 20:07:04 -0700

    intial commit

--- app/src/main/java/com/microacademylabs/themepicker/MainActivity.java
+++ app/src/main/java/com/microacademylabs/themepicker/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    
+
     btn = (Button) findViewById(R.id.changeButton);
     btn.setOnClickListener(new View.OnClickListener() {
       @Override
