3b9baaf1cd770f3500cb38c13a9accf189184e90
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-05-17 21:19:41 -0700

    random colors on button click - unexpected results

--- app/src/main/java/com/microacademylabs/themepicker/MainActivity.java
+++ app/src/main/java/com/microacademylabs/themepicker/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
   private Button btn;
   private int myTheme;
@@ -21,15 +23,76 @@ public class MainActivity extends AppCompatActivity {
     btn.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-
+        MainActivity.this.recreate();
       }
     });
   }
   @Override
   public Resources.Theme getTheme() {
+    Random rand = new Random();
+    int randTheme = rand.nextInt(18);
     Resources.Theme theme = super.getTheme();
-    theme.applyStyle(R.style.AppTheme2, true);
-
+    switch(randTheme) {
+      case 0:
+        theme.applyStyle(R.style.OrangeTheme, true);
+        break;
+      case 1:
+        theme.applyStyle(R.style.DeepOrangeTheme, true);
+        break;
+      case 2:
+        theme.applyStyle(R.style.YellowTheme, true);
+        break;
+      case 3:
+        theme.applyStyle(R.style.AmberTheme, true);
+        break;
+      case 4:
+        theme.applyStyle(R.style.BrownTheme, true);
+        break;
+      case 5:
+        theme.applyStyle(R.style.GreyTheme, true);
+        break;
+      case 6:
+        theme.applyStyle(R.style.BlueGreyTheme, true);
+        break;
+      case 7:
+        theme.applyStyle(R.style.LightBlueTheme, true);
+        break;
+      case 8:
+        theme.applyStyle(R.style.BlueTheme, true);
+        break;
+      case 9:
+        theme.applyStyle(R.style.OrangeTheme, true);
+        break;
+      case 10:
+        theme.applyStyle(R.style.IndigoTheme, true);
+        break;
+      case 11:
+        theme.applyStyle(R.style.PurpleTheme, true);
+        break;
+      case 12:
+        theme.applyStyle(R.style.DeepPurpleTheme, true);
+        break;
+      case 13:
+        theme.applyStyle(R.style.RedTheme, true);
+        break;
+      case 14:
+        theme.applyStyle(R.style.PinkTheme, true);
+        break;
+      case 15:
+        theme.applyStyle(R.style.CyanTheme, true);
+        break;
+      case 16:
+        theme.applyStyle(R.style.TealTheme, true);
+        break;
+      case 17:
+        theme.applyStyle(R.style.GreenTheme, true);
+        break;
+      case 18:
+        theme.applyStyle(R.style.LightGreenTheme, true);
+        break;
+      default:
+        break;
+    }
     return theme;
   }
 }
