48b78e9a7c57f5afbac95d5227ef04c94169f140
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2017-05-28 09:36:03 -0700

    adjust floating action button colors dynamically, improved constraint layout

--- app/src/main/java/com/microacademylabs/themepicker/MainActivity.java
+++ app/src/main/java/com/microacademylabs/themepicker/MainActivity.java
@@ -1,12 +1,17 @@
 package com.microacademylabs.themepicker;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.WindowDecorActionBar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -22,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
   private TextView tvDark;
   private TextView tvLight;
   private TextView tvPrimary;
+  private TextView tvAccent;
+  private int mAccent;
 
-  private Random rand = new Random();
   int thisTheme;
 
   @Override
@@ -33,11 +39,26 @@ public class MainActivity extends AppCompatActivity {
 
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    mAccent = thisTheme;
+
     tvTitle = (TextView) findViewById(R.id.greetingText);
     tvPrimary = (TextView) findViewById(R.id.primaryText);
+    tvAccent = (TextView) findViewById(R.id.accentText);
     tvDark = (TextView) findViewById(R.id.darkText);
     tvLight = (TextView) findViewById(R.id.lightText);
     fabAccent = (FloatingActionButton) findViewById(R.id.fabAccent);
+    fabAccent.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if(mAccent <18) {
+          mAccent++;
+        } else {
+          mAccent =0;
+        }
+        changeAccent(mAccent);
+      }
+    });
 
     btnNext = (Button) findViewById(R.id.nextButton);
     btnNext.setOnClickListener(new View.OnClickListener() {
@@ -75,10 +96,10 @@ public class MainActivity extends AppCompatActivity {
   private void setColors() {
     switch(thisTheme) {
       case 0:
-        newColors("#F5F5F5", "#616161", "#9E9E9E", "#9E9E9E", "Pink Theme");
+        newColors("#F5F5F5", "#616161", "#9E9E9E", "#9E9E9E", "Grey Theme");
         break;
       case 1:
-        newColors("#F8BBD0", "#C2185B", "#E91E63", "#E91E63", "Pink Theme");
+        newColors("#F8BBD0", "#C2185B", "#E91E63", "#FF4081", "Pink Theme");
         break;
       case 2:
         newColors("#B2EBF2", "#0097A7", "#00BCD4", "#00BCD4", "Cyan Theme");
@@ -96,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
         newColors("#F0F4C3", "#AFB42B", "#CDDC39", "#CDDC39", "Lime Theme");
         break;
       case 7:
-        newColors("#E1BEE7", "#7B1FA2", "#9C27B0", "#9C27B0", "Purple Theme");
+        newColors("#E1BEE7", "#7B1FA2", "#9C27B0", "#E040FB", "Purple Theme");
         break;
       case 8:
         newColors("#B3E5FC", "#0288D1", "#03A9F4", "#03A9F4", "Light Blue Theme");
@@ -105,19 +126,19 @@ public class MainActivity extends AppCompatActivity {
         newColors("#FFF9C4", "#FBC02D", "#FFEB3B", "#FFEB3B", "Yellow Theme");
         break;
       case 10:
-        newColors("#C5CAE9", "#303F9F", "#3F51B5", "#3F51B5", "Indigo Theme");
+        newColors("#C5CAE9", "#303F9F", "#3F51B5", "#536DFE", "Indigo Theme");
         break;
       case 11:
         newColors("#FFCCBC", "#E64A19", "#FF5722", "#FF5722", "Deep Orange Theme");
         break;
       case 12:
-        newColors("#BBDEFB", "#2196F3", "#1976D2", "#1976D2", "Blue Theme");
+        newColors("#BBDEFB", "#2196F3", "#1976D2", "#448AFF", "Blue Theme");
         break;
       case 13:
         newColors("#DCEDC8", "#689F38", "#8BC34A", "#8BC34A", "Light Green Theme");
         break;
       case 14:
-        newColors("#D1C4E9", "#512DA8", "#673AB7", "#673AB7", "Deep Purple Theme");
+        newColors("#D1C4E9", "#512DA8", "#673AB7", "#7C4DFF", "Deep Purple Theme");
         break;
       case 15:
         newColors("#FFE0B2", "#F57C00", "#FF9800", "#FF9800", "Orange Theme");
@@ -129,24 +150,48 @@ public class MainActivity extends AppCompatActivity {
         newColors("#CFD8DC", "#455A64", "#607D8B", "#607D8B", "Blue Grey Theme");
         break;
       case 18:
-        newColors("#FFCDD2", "#D32F2F", "#F44336", "#F44336", "Red Theme");
+        newColors("#FFCDD2", "#D32F2F", "#F44336", "#FF5252", "Red Theme");
         break;
       default:
         break;
     }
   }
 
+  private void changeAccent(int c) {
+    String[]accentColor = new String[] {
+        "#9E9E9E", "#FF4081", "#00BCD4", "#FFC107", "#4CAF50", "#795548",
+        "#CDDC39", "#E040FB", "#03A9F4", "#FFEB3B", "#536DFE", "#FF5722",
+        "#448AFF", "#8BC34A", "#7C4DFF", "#FF9800", "#009688", "#607D8B", "#FF5252"
+    };
+    fabAccent.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor(accentColor[c])));
+    tvAccent.setText("Accent: " + accentColor[c]);
+    tvAccent.setTextColor(Color.parseColor(accentColor[c]));
+  }
+
   private void newColors(String light, String dark, String primary, String accent, String themeName) {
     tvTitle.setText(themeName);
-    super.getTheme().applyStyle(R.style.GreyTheme, true);
-    tvDark.setText("Dark: " + dark);
     tvTitle.setTextColor(Color.parseColor(dark));
-    tvPrimary.setText("Primary: " + primary);
-    tvLight.setText("Light: " + light);
     tvTitle.setBackgroundColor(Color.parseColor(light));
-    fabAccent.setBackgroundColor(Color.parseColor(accent));
-    btnNext.setBackgroundColor(Color.parseColor(accent));
-    btnPrev.setBackgroundColor(Color.parseColor(accent));
+    tvDark.setText("Dark: " + dark);
+    tvDark.setTextColor(Color.parseColor(dark));
+    tvPrimary.setText("Primary: " + primary);
+    tvPrimary.setTextColor(Color.parseColor(primary));
+    fabAccent.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor(accent)));
+    tvAccent.setText("Accent: " + accent);
+    tvAccent.setTextColor(Color.parseColor(accent));
+    tvLight.setText("Light: " + light);
+    tvLight.setTextColor(Color.parseColor(light));
+    tvLight.setBackgroundColor(Color.parseColor(dark));
+
+    btnNext.setBackgroundColor(Color.parseColor(dark));
+    btnPrev.setBackgroundColor(Color.parseColor(dark));
+
+    // FAILED EFFORTS TO CHANGE THEME DYNAMICALLY...
+    //    fabAccent.setBackgroundColor(Color.parseColor(accent));//not working, theme seems to over-ride this setting
+    //    fabAccent.setBackgroundDrawable(new ColorDrawable(Color.parseColor(accent)));
+    //    getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor(primary)));
+    //    getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor(dark)));
+    //    getWindow().setBackgroundDrawable(new ColorDrawable(Color.parseColor(dark)));
   }
 
   @Override
